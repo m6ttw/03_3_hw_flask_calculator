3ecc1224ab59eab0e75062e6d35fb08a178fc7b8
m6ttw <matthewrhyswilliams@live.com>
2020-12-02 23:20:11 +0000

    added changes

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,18 +1,22 @@
 from app import app
 from modules.calculator import *
 
+@app.route("/")
+def index():
+    return "Hello, World!"
+
 @app.route("/add/<number_1>/<number_2>")
-def add_in_browser(number1, number2):
-    return f"The answer is {add(int(number1), int(number2))}"
+def add_in_browser(number_1, number_2):
+    return f"The answer is {add(int(number_1), int(number_2))}"
 
-@app.route("/subtract/<number1>/<number2>")
-def subtract_in_browser(number1, number2):
-    return f"The answer is {subtract(int(number1), int(number2))}"
+@app.route("/subtract/<number_1>/<number_2>")
+def subtract_in_browser(number_1, number_2):
+    return f"The answer is {subtract(int(number_1), int(number_2))}"
 
-@app.route("/multiply/<number1>/<number2>")
-def multiply_in_browser(number1, number2):
-    return f"The answer is {multiply(int(number1), int(number2))}"
+@app.route("/multiply/<number_1>/<number_2>")
+def multiply_in_browser(number_1, number_2):
+    return f"The answer is {multiply(int(number_1), int(number_2))}"
 
-@app.route("/subtract/<num1>/<num2>")
-def divide_in_browser(number1, number2):
-    return f"The answer is {divide(int(number1), int(number2))}"
+@app.route("/subtract/<number_1>/<number_2>")
+def divide_in_browser(number_1, number_2):
+    return f"The answer is {divide(int(number_1), int(number_2))}"
