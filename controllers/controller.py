dd99a3b62eb26a20db73eff30214496b997697b9
m6ttw <matthewrhyswilliams@live.com>
2020-12-02 23:22:32 +0000

    final changes

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,17 +6,17 @@ def index():
     return "Hello, World!"
 
 @app.route("/add/<number_1>/<number_2>")
-def add_in_browser(number_1, number_2):
+def add_in_app(number_1, number_2):
     return f"The answer is {add(int(number_1), int(number_2))}"
 
 @app.route("/subtract/<number_1>/<number_2>")
-def subtract_in_browser(number_1, number_2):
+def subtract_in_app(number_1, number_2):
     return f"The answer is {subtract(int(number_1), int(number_2))}"
 
 @app.route("/multiply/<number_1>/<number_2>")
-def multiply_in_browser(number_1, number_2):
+def multiply_in_app(number_1, number_2):
     return f"The answer is {multiply(int(number_1), int(number_2))}"
 
 @app.route("/subtract/<number_1>/<number_2>")
-def divide_in_browser(number_1, number_2):
+def divide_in_app(number_1, number_2):
     return f"The answer is {divide(int(number_1), int(number_2))}"
